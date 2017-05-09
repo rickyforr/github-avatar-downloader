77418817bd4aa5535c90f00abdb795f835455c55
Richard Forrester <richard.forrester@gmail.com>
2017-05-09 15:39:48 +0000

    modified .pipe to download avatars in a loop with the login values as the image name

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs')
 
 var options = {
   url: 'https://api.github.com/repos/jquery/jquery/contributors',
@@ -8,14 +9,39 @@ var options = {
 };
 
 function callback(error, response, body) {
+  var results = []
+
+
   if (!error && response.statusCode == 200) {
     var info = JSON.parse(body);
     console.log(info.length);
+
+
   for (var i = 0; i < info.length; i++) {
-    console.log(info[i].avatar_url)
+
+    logins = info[i].login
+
+    var avatars = request.get(info[i].avatar_url)
+    .on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+        console.log('Response Status Code: ', response.statusCode);
+       })
+       .pipe(fs.createWriteStream('./' + logins + '.jpeg'));
+  }
   }
 
-  }
+
+
+
 }
+
+
+
+
+
+
+
 
 request(options, callback);
