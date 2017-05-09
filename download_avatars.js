4348af84304c386f74ce8521fbb07aba1d2ea3c2
Richard Forrester <richard.forrester@gmail.com>
2017-05-09 01:58:26 +0000

    hard coded function to get url with data in json format

--- download_avatars.js
+++ download_avatars.js
@@ -2,10 +2,18 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = "rickyforr";
+var GITHUB_TOKEN = "5e55d4be18de888bdebbfee138b5ac39a63f2c87";
+
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
-  getRepoContributors("jquery", "jquery", function(err, result) {
+   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+   console.log(requestURL);
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
-}
