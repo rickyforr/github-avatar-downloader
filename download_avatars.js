68665d8742dbcf7aa194cccbd879ef64a164ce4e
Richard Forrester <richard.forrester@gmail.com>
2017-05-09 05:08:18 +0000

    able to print avatar url's to console

--- download_avatars.js
+++ download_avatars.js
@@ -1,27 +1,21 @@
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+var options = {
+  url: 'https://api.github.com/repos/jquery/jquery/contributors',
+  headers: {
+    'User-Agent': 'request'
+  }
+};
 
-var GITHUB_USER = "rickyforr";
-var GITHUB_TOKEN = "5e55d4be18de888bdebbfee138b5ac39a63f2c87";
+function callback(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var info = JSON.parse(body);
+    console.log(info.length);
+  for (var i = 0; i < info.length; i++) {
+    console.log(info[i].avatar_url)
+  }
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
-
-  request.get('https://api.github.com/repos/jquery/jquery/contributors')
-
-   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-
-   console.log(requestURL);
-
- request.get('https://api.github.com/repos/jquery/jquery/contributors')
-  .on('response', function (response) {
-         console.log('Response Status Code: ', response);
-       })
-
+  }
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
+request(options, callback);
